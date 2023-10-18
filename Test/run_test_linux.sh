d495a030465f3cebd1d8e8024928bd51614bc5ef
Baruch Baksht <bakshtb@gmail.com>
2023-10-18 13:25:33 +0300

    fix bug

--- Test/run_test_linux.sh
+++ Test/run_test_linux.sh
@@ -3,12 +3,12 @@
 echo "----------------------------------------------------"
 echo "Copying files to the test folder..."
 cp ../Release/Rectangle.py .
-cp ../Release/Release/_Rectangle.so .
+cp ../Release/_Rectangle.so .
 
 echo "Running the test script..."
 echo "----------------------------------------------------"
 
-python test.py
+python3 test.py
 
 echo "----------------------------------------------------"
 echo "Done!"
