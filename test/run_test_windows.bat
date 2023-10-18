cea38aefa6b3a4a70afe41ca2807f21bd0efc765
Baruch Baksht <bakshtb@gmail.com>
2023-10-18 21:45:16 +0300

    update test

--- test/run_test_windows.bat
+++ test/run_test_windows.bat
@@ -1,8 +1,8 @@
 @echo off
 echo ----------------------------------------------------
 echo Copying files to the test folder...
-xcopy /Y ..\Release\Rectangle.py . > nul
-xcopy /Y ..\Release\Release\_Rectangle.pyd . > nul
+xcopy /Y ..\output\Rectangle.py . > nul
+xcopy /Y ..\output\_Rectangle.pyd . > nul
 
 echo Running the test script...
 
