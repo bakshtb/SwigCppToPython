c213343a5aff4a201ee9571d9bd6975abb269735
Baruch Baksht <bakshtb@gmail.com>
2023-10-18 13:16:25 +0300

    add linux test script

---
+++ Test/run_test_linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "----------------------------------------------------"
+echo "Copying files to the test folder..."
+cp ../Release/Rectangle.py .
+cp ../Release/Release/_Rectangle.pyd .
+
+echo "Running the test script..."
+echo "----------------------------------------------------"
+
+python test.py
+
+echo "----------------------------------------------------"
+echo "Done!"
+echo "----------------------------------------------------"
